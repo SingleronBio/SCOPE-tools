8de7d59797ff36242f5c6d764786899d751d698c
luyang <510426762@qq.com>
2019-09-10 11:04:05 +0800

    format and specify utf-8 encoding

--- scopetools/__main__.py
+++ scopetools/__main__.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+
+
 from scopetools import cli
 
 cli.cli()
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,8 @@
+# -*- coding: utf-8 -*-
+
+
 from scopetools import cli
+
 
 def test_cli_template():
     assert cli.cli() is None
