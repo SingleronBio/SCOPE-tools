5880429b02e57998c876e67e179efe2e65f6e4a4
luyang93 <luyang@singleronbio.com>
2020-05-13 13:49:56 +0800

    add cached_property

--- scopetools/utils.py
+++ scopetools/utils.py
@@ -164,6 +164,25 @@ class CommandWrapper(object):
             self.returncode = self.p.returncode
 
 
+class cached_property(object):
+    """ A property that is only computed once per instance and then replaces
+        itself with an ordinary attribute. Deleting the attribute resets the
+        property.
+
+        Source: https://github.com/bottlepy/bottle/commit/fa7733e075da0d790d809aa3d2f53071897e6f76
+        """
+
+    def __init__(self, func):
+        self.__doc__ = getattr(func, '__doc__')
+        self.func = func
+
+    def __get__(self, obj, cls):
+        if obj is None:
+            return self
+        value = obj.__dict__[self.func.__name__] = self.func(obj)
+        return value
+
+
 def getlogger(name=__name__):
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     data_fmt = '%y-%m-%d %H:%M:%S'
